80d15ec52d53c75ffdf1c90c33a6834fa96d8b90
Hevida Nur Uludağ <n.uludag@media.local>
2024-11-01 18:18:01 +0300

    guncelleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,61 @@
-<<<<<<< HEAD
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-=======
-import logo from './logo.svg';
 import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick"; // react-slick'i ekliyoruz
 import { FaWhatsapp } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
-import {AppBar, Box, Button, Card, CardContent, IconButton, textFieldClasses, Toolbar, Typography} from "@mui/material";
-import { useState } from "react";
-import { pink } from '@mui/material/colors';
-
->>>>>>> 0ed444c4c3003b1c97dcf4005dda2343994d5d40
+import {
+    AppBar,
+    Box,
+    Button,
+    Card,
+    CardContent,
+    Toolbar
+} from "@mui/material";
 
 function App() {
+    const [isSpan, setIsSpan] = useState(false); // isSpan durumunu tanımladık
     const isLoggedIn = localStorage.getItem("isLoggedIn");
-    console.log(isLoggedIn , 'DENEME')
+    console.log(isLoggedIn, 'DENEME');
+
+    const changeSpan = () => {
+        setIsSpan(!isSpan);
+    };
+
+    const settings = {
+        // Slider ayarları
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
 
     return (
-        <Routes>
-            {/* Eğer kullanıcı giriş yaptıysa Home'a yönlendirilir, aksi halde Login gösterilir */}
-            {/*<Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />*/}
-            <Route path="/" element={ <Home /> } />
-
-<<<<<<< HEAD
-            <Route path="/login" element={ <Login />} />
-        </Routes>
-=======
+        <Box>
+            {/* Navigasyon ve AppBar */}
+            <AppBar position="static">
+                <Toolbar>
                     <div style={{ borderRadius: "5px", margin: "1%", color: "deeppink" }}>
-                        <a href="#hakkımda" sx={{ color: "deeppink" }}>Hakkımda</a>
+                        <a href="#hakkımda" style={{ color: "white", textDecoration: "none" }}>Hakkımda</a>
                     </div>
-
                     <div style={{ borderRadius: "5px", margin: "1%", color: "deeppink" }}>
-                        <a href="#galeri" sx={{ color: "deeppink" }}>Galeri</a>
+                        <a href="#galeri" style={{ color: "white", textDecoration: "none" }}>Galeri</a>
                     </div>
-
                     <div style={{ borderRadius: "5px", margin: "1%", color: "deeppink" }}>
-                        <a href="#anasayfa" sx={{ color: "deeppink" }}>iletişim</a>
+                        <a href="#iletisim" style={{ color: "white", textDecoration: "none" }}>İletişim</a>
                     </div>
-
-                    <a href="#hebidayagit">Hebida</a>
+                    <div style={{ marginLeft: "auto", marginRight: "1%" }}>
+                        <a href="#hebidayagit" style={{ color: "white", textDecoration: "none" }}>Hebida</a>
+                    </div>
                 </Toolbar>
             </AppBar>
+
+            {/* Başlık */}
             <Box sx={{
                 width: "100%",
                 backgroundColor: "deeppink",
@@ -55,7 +65,11 @@ function App() {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center"
-            }}><span>Hevida Nur Uludağ</span></Box>
+            }}>
+                <span>Hevida Nur Uludağ</span>
+            </Box>
+
+            {/* Slider */}
             <Box sx={{ width: "90%", margin: "0 auto", marginTop: "20px" }}>
                 <Slider {...settings} id="anasayfa">
                     <div>
@@ -69,6 +83,8 @@ function App() {
                     </div>
                 </Slider>
             </Box>
+
+            {/* Hakkımda Bölümü */}
             <Box sx={{
                 width: "100%",
                 marginTop: "20px",
@@ -95,85 +111,81 @@ function App() {
                         height: "70%",
                         borderRadius: "10px"
                     }}>
-                        <img src="/hevidapr.png" style={{ width: "100%", height: "100%", borderRadius: "10px" }} />
+                        <img src="/hevidapr.png" style={{ width: "100%", height: "100%", borderRadius: "10px" }} alt="Profil" />
                     </CardContent>
                     <CardContent id="hakkımda" sx={{ height: "90%", width: "70%" }}>
                         <Box>
-                            <h1 style={{Color:"red"}}>Hakkımda</h1>
-                            <h6 id={"haltı"}>
+                            <h1 style={{ color: "red" }}>Hakkımda</h1>
+                            <h6 id="haltı">
                                 Merhaba! Ben Hevida, JavaScript alanında uzmanlaşmak isteyen bir blog yazarıyım. Bu blogda, hem yazılım hem de kendi yaşantım hakkında yazılar paylaşacağım.
-                        </h6>
+                            </h6>
                         </Box>
                     </CardContent>
                 </Card>
             </Box>
 
+            {/* Galeri Bölümü */}
+            <Box id="galeri" sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+                <Card sx={{ margin: "10px", width: "30%" }}>
+                    <CardContent sx={{ backgroundColor: "deeppink", textAlign: "center" }}>
+                        <h3 id="h3style">img 1</h3>
+                        <img src="/img1.jpg" id="hebidaImgStyle" style={{ width: "100%" }} alt="img1" />
+                    </CardContent>
+                </Card>
 
+                <Card sx={{ margin: "10px", width: "30%" }}>
+                    <CardContent sx={{ backgroundColor: "deeppink", textAlign: "center" }}>
+                        <h3 id="h3style">img 2</h3>
+                        <img src="/tavsan.jpg" id="hebidaImgStyle" style={{ width: "100%" }} alt="tavsan" />
+                    </CardContent>
+                </Card>
 
-            <Card id="galeri">
-                <CardContent sx={{ height: "30%", width: "30%", alignItems: "center", backgroundColor: "deeppink" }}>
-                    <h3 id="h3style">img 1</h3>
-                    <img src="/img1.jpg" id="hebidaImgStyle" />
-                </CardContent>
-            </Card>
+                <Card sx={{ margin: "10px", width: "30%" }}>
+                    <CardContent sx={{ backgroundColor: "deeppink", textAlign: "center" }}>
+                        <h3 id="h3style">img 3</h3>
+                        <img src="/img3.jpg" id="hebidaImgStyle" style={{ width: "100%" }} alt="img3" />
+                    </CardContent>
+                </Card>
 
-            <br></br>
+                <Card sx={{ margin: "10px", width: "30%" }}>
+                    <CardContent sx={{ backgroundColor: "deeppink", textAlign: "center" }}>
+                        <h3 id="h3style">img 4</h3>
+                        <img src="/img2.jpg" id="hebidaImgStyle" style={{ width: "100%" }} alt="img2" />
+                    </CardContent>
+                </Card>
+            </Box>
 
-            <Card>
-                <CardContent sx={{ height: "30%", width: "30%", alignItems: "center", backgroundColor: "deeppink" }}>
-                    <h3 id="h3style">img 2</h3>
-                    <img src="/tavsan.jpg" id="hebidaImgStyle" />
-                </CardContent>
-            </Card>
-
-
-            <br></br>
-
-            <Card id="hebidayagit">
-                <CardContent sx={{ height: "30%", width: "30%", alignItems: "center", backgroundColor: "deeppink" }}>
-                    <h3 id="h3style">img 3</h3>
-                    <img src="/img3.jpg" id="hebidaImgStyle" />
-                </CardContent>
-            </Card>
-
-            <br></br>
-            <Card id="hebidayagit">
-                <CardContent sx={{ height: "30%", width: "30%", alignItems: "center", backgroundColor: "deeppink" }}>
-                    <h3 id="h3style">img 3</h3>
-                    <img src="/img2.jpg" id="hebidaImgStyle" />
-                </CardContent>
-            </Card>
-
-            <footer>
-
-                <Box sx={{ marginTop: "20px" }}>
+            {/* Footer Bölümü */}
+            <footer id="iletisim">
+                <Box sx={{ marginTop: "20px", textAlign: "center" }}>
                     {isSpan && (
-                        <Box sx={{marginTop: "10px"}}>
-                            <a id={"aetiketi"} href="https://www.instagram.com/hevidanur_uludag/">
-                                <BsInstagram style={{marginRight: "8px"}}/>Instagram</a>
+                        <Box sx={{ marginTop: "10px" }}>
+                            <a id="aetiketi" href="https://www.instagram.com/hevidanur_uludag/" style={{ marginRight: "8px", color: "deeppink" }}>
+                                <BsInstagram style={{ marginRight: "8px" }} />Instagram
+                            </a>
 
-                            <a id={"aetiketi2"}
-                               href="https://www.google.com/search?q=whatsapp&rlz=1C1GCEU_trTR1129TR1129&oq=whatsapp&gs_lcrp=EgZjaHJvbWUyDwgAEEUYORiDARixAxiABDIMCAEQIxgnGIAEGIoFMg0IAhAAGIMBGLEDGIAEMgoIAxAAGLEDGIAEMg0IBBAAGIMBGLEDGIAEMg0IBRAAGIMBGLEDGIAEMgYIBhBFGDwyBggHEAUYQNIBCDI3NTFqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8">
-                                <FaWhatsapp style={{marginRight: "8px"}}/>Whatsapp</a>
+                            <a id="aetiketi2" href="https://www.whatsapp.com/" style={{ color: "deeppink" }}>
+                                <FaWhatsapp style={{ marginRight: "8px" }} />Whatsapp
+                            </a>
                         </Box>
                     )}
                     <Button onClick={changeSpan} sx={{ color: "deeppink" }}>
                         {isSpan ? 'Gizle' : 'Daha fazla oku'}
                     </Button>
                 </Box>
-
-
             </footer>
-
         </Box>
->>>>>>> 0ed444c4c3003b1c97dcf4005dda2343994d5d40
     );
 }
 
 export default function Root() {
     return (
         <Router>
-            <App />
+            <Routes>
+                {/* Eğer kullanıcı giriş yaptıysa Home'a yönlendirilir, aksi halde Login gösterilir */}
+                <Route path="/" element={localStorage.getItem("isLoggedIn") ? <App /> : <Navigate to="/login" />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </Router>
     );
 }
